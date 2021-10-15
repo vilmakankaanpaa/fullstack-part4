5cee7592004192bf341dba3a17a32b44a410db60
Vilma Kankaanpää <vilma.kankaanpaa@aalto.fi>
2021-10-15 16:32:51 +0300

    Exercise 4.5. favoriteBlog test

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,18 @@ const totalLikes = (blogs) => {
   return blogs.reduce(counter, 0)
 }
 
+const favoriteBlog = (blogs) => {
+
+  const reducer = (favorite, blog) => {
+    const p = favorite ? favorite.likes : 0
+    const v = blog.likes
+    return ( p < v ? blog : favorite )
+  }
+
+  return blogs.reduce(reducer, undefined)
+}
+
 module.exports = {
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
